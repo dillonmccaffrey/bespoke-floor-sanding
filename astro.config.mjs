bfe2c7526737008e368c6ce0491d3407a0fceb40
Dillon Mc Caffrey <me@dillonmccaffrey.dev>
2026-01-22 17:06:20 +0000

    feat: US-014 - Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bespokefloorsanding.ie',
   output: 'static',
+
   adapter: node({
     mode: 'standalone'
   }),
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
