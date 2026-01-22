a2ae7c37771adc279c0aeec710a6571d6ca615db
Dillon Mc Caffrey <me@dillonmccaffrey.dev>
2026-01-22 19:05:21 +0000

    config: Update site URL to bespoke.dillonmccaffrey.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bespokefloorsanding.ie',
+  site: 'https://bespoke.dillonmccaffrey.dev',
   output: 'static',
   trailingSlash: 'ignore',
 
---
+++ src/content/testimonials/map-rating-5-order-1-name-olivia-location-monaghan-quote-“nygell-showed-up-on-time-and-did-an-excellent-job-sanding-and-refinishing-our-living-room-floors-the-work-was-done-efficiently-and-looks-great—highly-recommended-”.md
@@ -0,0 +1,9 @@
+---
+name: Olivia
+location: Monaghan
+quote: “Nygell showed up on time and did an excellent job sanding and
+  refinishing our living room floors. The work was done efficiently and looks
+  great—highly recommended!”
+rating: 5
+order: 2
+---
